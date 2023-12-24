d26725b7b131637804ac91c3bf29eea302520c73
dleunji <lej970703@naver.com>
2023-12-25 02:09:42 +0900

    make comments for understadning

--- cuda_src/SudokuSolver_CudaBruteForce_kernel.cu
+++ cuda_src/SudokuSolver_CudaBruteForce_kernel.cu
@@ -8,8 +8,12 @@ __global__ void backtrack_kernel(int n, int N, int *boards, int *found)
 {
   // use shared memory
   extern __shared__ int shared[];
-  int board_num = boards[blockIdx.x * N * N + (threadIdx.x * N) + threadIdx.y];
+  int tid = blockIdx.x * N * N + (threadIdx.x * N) + threadIdx.y;
+  // board_num = chosen number of the current position
+  int board_num = boards[tid];
+  // size = N, keep track of numbers that have been tried and failed for a particular position
   int *failed = shared + 4;
+  // size = N * N
   int *stack = shared + N + 4;
   int top = 0;
   // put all locations of empty tiles in stack
@@ -29,87 +33,121 @@ __global__ void backtrack_kernel(int n, int N, int *boards, int *found)
   __syncthreads();
 
   int last_op = 0; // 0 - push stack, 1 - pop stack
+  if (blockIdx.x == 0 && threadIdx.x == 0 && threadIdx.y == 0)
+  {
+    printf("top: %d\n", top);
+  }
   while (*found == -1)
   {
     if (threadIdx.x == 0 && threadIdx.y == 0)
     {
+      // position -> shared[0], number -> shared[1]
       int stack_num = stack[top];
+      if (blockIdx.x == 0)
+      {
+        printf("stack num : %d\n", stack_num);
+      }
       shared[0] = stack_num % (N * N); // communicate nowp
       if (last_op == 0)
       {
-        // first check if the board is filled
+        // first check if the board is filled (empty queue)
         if (stack_num == -1)
         {
           // answer found
           atomicCAS(found, -1, blockIdx.x);
-          if (*found == blockIdx.x && threadIdx.x == 0 && threadIdx.y == 0)
-          {
-            for (int i = 0; i < N; i++)
-            {
-              for (int j = 0; j < N; j++)
-              {
-                printf("%d ", boards[blockIdx.x * N * N + i * N + j]);
-              }
-              printf("\n");
-            }
-          }
           shared[0] = -1;
         }
-        // else initialize the number to try
+        // else initialize the number to try (new position)
         shared[1] = 1;
       }
       else
       {
+        // the number is saved in zero-base, but used in one-base
         shared[1] = stack_num / (N * N) + 1;
       }
     }
+
+    // initialize failed array whose size of N
     if (threadIdx.y == 0)
       failed[threadIdx.x] = 0;
     __syncthreads();
 
     // find next valid number
+    // nowp = the position to check in this board(block)
     int nowp = shared[0], i = shared[1];
+    if (blockIdx.x == 0 && threadIdx.x == 0 && threadIdx.y == 0)
+    {
+      printf("CURR: i = %d nowp = %d\n", i, nowp);
+    }
     if (nowp == -1)
       break;
+
+    // temp for saving the `i`
     int num_to_try = i;
+    // each thread checks whether the current position (`nowp`) is in it respective row, column, or box.
+    // In case of the threads in the row, col, box including the `nowp`
     if (ROW(nowp, N) == threadIdx.x || COL(nowp, N) == threadIdx.y || BOX(nowp, n) == box_now)
     {
       for (; i <= N; ++i)
         if (i == board_num)
           failed[i - 1] = 1;
     }
+    if (blockIdx.x == 0 && threadIdx.x == 0 && threadIdx.y == 0)
+    {
+      printf("compare with board_num: %d\n", board_num);
+      for (int i = 0; i < N; i++)
+      {
+        printf("%d ", failed[i]);
+      }
+      printf("\n");
+    }
+    // As the result, `i`  = has been tried and failed at this position
     __syncthreads();
 
     if (threadIdx.x == 0 && threadIdx.y == 0)
     {
+      // i have not been tried
       for (i = num_to_try; i <= N && failed[i - 1] != 0; ++i)
         ;
       shared[2] = nowp; // record in shared memory for communication
       if (i <= N)
       {
         // push stack
+        // push `i`(number) and `nowp`(position) together
         stack[top++] = i * N * N + nowp;
         shared[3] = i;
         last_op = 0;
+        if (blockIdx.x == 0)
+        {
+          printf("PUSH: i = %d nowp = %d\n", i, nowp);
+        }
       }
       else
       {
         // pop stack
         if (top == 0)
+          // the stack becomes empty!
           shared[2] = -1;
         stack[top--] = nowp;
         shared[3] = 0;
         last_op = 1;
+        if (blockIdx.x == 0)
+        {
+          printf("PUSH: %d\n", nowp);
+        }
       }
     }
     __syncthreads();
     if (shared[2] == -1)
       break;
     // update local register of last step
+    // shared[3] matches with the position nowp = shared[2]
     if (threadIdx.x * N + threadIdx.y == shared[2])
+      // shared[3] = the value of latest operation.
       board_num = shared[3];
   }
 
+  // after cooperation of threads in the block, the final chose board_num is mapped to position
   if (*found == blockIdx.x)
     boards[blockIdx.x * N * N + (threadIdx.x * N) + threadIdx.y] = board_num;
 }
